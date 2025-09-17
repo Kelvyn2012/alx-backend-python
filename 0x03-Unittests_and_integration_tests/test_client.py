c27ad4efcd25acae326db85d0d75bc51abb19912
kelvyn2012 <kudufo@gmail.com>
2025-09-17 21:48:57 +0100

    Created the TestIntegrationGithubOrgClient(unittest.TestCase) class and implement the setUpClass and tearDownClass

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,8 @@
 
 import unittest
 from unittest.mock import patch, PropertyMock
-from parameterized import parameterized, parameterized_class
+from parameterized import parameterized
+from parameterized import parameterized_class
 
 from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
