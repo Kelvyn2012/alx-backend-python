385e2dc2b0f5a8054c77402faf756dbb5501342b
kelvyn2012 <kudufo@gmail.com>
2025-09-22 21:26:22 +0100

    create and extended my permission and apply permission to my viewset to enforce access control

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,14 +1,20 @@
 from rest_framework import permissions
 
 
-class IsParticipantOrReadOnly(permissions.BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
     """
     Allow access only to participants of a conversation.
     """
 
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        if hasattr(obj, "participants"):  # Conversation
+        # Conversation object
+        if hasattr(obj, "participants"):
             return request.user in obj.participants.all()
-        if hasattr(obj, "conversation"):  # message
+
+        # Message object
+        if hasattr(obj, "conversation"):
             return request.user in obj.conversation.participants.all()
         return False
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -3,16 +3,22 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
+from .permissions import IsParticipantOfConversation
+from rest_framework.permissions import IsAuthenticated
 
 User = get_user_model()
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
     search_fields = ["title", "participants__username"]
     ordering_fields = ["created_at"]
+
+    def get_queryset(self):
+        # Only return conversations the user is part of
+        return Conversation.objects.filter(participants=self.request.user)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -23,11 +29,15 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ["content", "sender__username"]
     ordering_fields = ["timestamp"]
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+
+    def get_queryset(self):
+        # Only return messages in conversations the user participates in
+        return Message.objects.filter(conversation__participants=self.request.user)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
